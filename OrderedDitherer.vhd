ccb2fd2cee3f77a91592b563ecc6b09d476cbd63
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-08-07 18:20:09 -0300

    feat: Complete Ordered Dithering pattern

--- OrderedDitherer.vhd
+++ OrderedDitherer.vhd
@@ -4,6 +4,17 @@ USE ieee.numeric_std.ALL;
 
 -- Given an 8-bit pixel located in row and column position,
 -- return the dithered color of the pixel (1-bit).
+--
+-- Uses the following 2x2 dithering matrix:
+--
+--   64  128
+--   192 0
+--
+-- The dithering is done by comparing the pixel value with the
+-- dithering matrix entry. If the pixel value is greater than the
+-- matrix entry, the pixel is dithered to white; otherwise it is
+-- dithered to black.
+--
 ENTITY OrderedDitherer IS
     PORT (
         pixel : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
@@ -21,7 +32,11 @@ ARCHITECTURE rtl OF OrderedDitherer IS
 BEGIN
     pixel_value <= to_integer(unsigned(pixel));
 
-    dithered_pixel <= '1' WHEN (row MOD 2 = 0) AND (column MOD 2 = 0) AND pixel_value > 0 ELSE
+    dithered_pixel <=
+        '1' WHEN (row MOD 2 = 0) AND (column MOD 2 = 0) AND pixel_value > 0 ELSE
+        '1' WHEN (row MOD 2 = 1) AND (column MOD 2 = 1) AND pixel_value > 64 ELSE
+        '1' WHEN (row MOD 2 = 1) AND (column MOD 2 = 0) AND pixel_value > 128 ELSE
+        '1' WHEN (row MOD 2 = 0) AND (column MOD 2 = 1) AND pixel_value > 192 ELSE
         '0';
 
 END ARCHITECTURE;
